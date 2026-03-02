e02ac326ac8134456bc985b1de914471046c6540
DanielStankovic <danielstankovic1992@gmail.com>
2026-03-02 10:11:52 +0100

    Test comment to trigger workflow

--- app/src/main/java/com/dsapps2018/dota2guessthesound/di/AppModule.kt
+++ app/src/main/java/com/dsapps2018/dota2guessthesound/di/AppModule.kt
@@ -14,6 +14,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    //test comment
     @Provides
     @Singleton
     fun provideSoundPlayer(@ApplicationContext context: Context) =
